a931dff3fe043c1711df895c7321222d0052076f
XiangChang <changxiangchina@outlook.com>
2025-04-08 03:43:40 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-/**
- * For Flexible_jaws controller
- * by X.Chang@Version1.0
- * LAB_121
- * 参数笔记
- * 最大张角对应传感器机械角度 0rad
- * 最小张角对应传感器机械角度 90rad
- * @version: v2.1.2
- */
-
 #include <SimpleFOC.h>
 #include <iostream>
 #include <vector>
@@ -26,7 +16,7 @@ BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27, 33); // 绑定三相电机驱
 // const int TX1 = 18;
 // const int RX1 = 17;
 
-// dirction
+// direction
 const int left_hand = -1;
 
 // 角度设置变量
@@ -34,57 +24,24 @@ float target = 0; // 目标
 float angle_limit = 100;
 int counter = 0;
 
-// 命令解释器的初始化
-Commander command = Commander(Serial);
-void doTarget(char *cmd) { command.scalar(&target, cmd); }
-void onMotor(char *cmd) { command.motor(&motor, cmd); }
-// 自定义函数
-int32_t decodeToTwosComplement(const std::vector<uint8_t> &byteList) // decode
-{
-    if (byteList.size() < 4)
-    {
-        std::cerr << "Error: Byte list must contain at least 4 bytes." << std::endl;
-        return 0;
+// 速度允差
+const float velocity_threshold = 1;
+const float angle_threshold = 200;
+
+// Control
+float error = 0;
+float torque_limit = 0;
+float angle_target = 0;
+
+// FOC 任务函数
+void focTask(void *pvParameters) {
+    while (1) {
+        motor.loopFOC();  // 在独立线程中运行 motor.loopFOC
     }
-
-    // 将字节序列直接解释为一个32位有符号整数
-    int32_t number;
-    memcpy(&number, byteList.data(), sizeof(number));
-
-    return number;
 }
 
-std::vector<uint8_t> getSubVector(const std::vector<uint8_t> &originalVector, int start, int end)
-{ // 用来分割vector
-    // 请注意，end 是不包含在内的，所以实际上是到 end - 1 的元素
-    return std::vector<uint8_t>(originalVector.begin() + start, originalVector.begin() + end);
-}
-
-std::vector<int32_t> getModeSpeed(const std::vector<uint8_t> &originalVector)
-{
-    std::vector<int32_t> res = {};
-    std::vector<uint8_t> originalVector1 = getSubVector(originalVector, 8, 12);
-    std::vector<uint8_t> originalVector2 = getSubVector(originalVector, 12, 16);
-    int32_t mode = decodeToTwosComplement(originalVector1);
-    int32_t speed = decodeToTwosComplement(originalVector2);
-    res.push_back(mode);
-    res.push_back(speed);
-
-    return res;
-}
-
-bool checkId(const std::vector<uint8_t> &originalVector)
-{
-    if (originalVector[7] == 0x14)
-    {
-        return true;
-    }
-    else
-        return false;
-}
 void setup()
 {
-
     // 传感器
     sensor.init();             // 初始化磁传感器
     motor.linkSensor(&sensor); // 将电机与传感器连接
@@ -98,96 +55,81 @@ void setup()
     motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
 
     // 运动控制环的配置
-    // motor.controller = MotionControlType::velocity; // 速度环
     motor.controller = MotionControlType::torque; // 力矩环
 
     // 控制部分的配置
-    // default parameters in defaults.h
-
-    // velocity PI control
     motor.PID_velocity.P = 0.2;
     motor.PID_velocity.I = 2;
-    // max velocity
-    motor.voltage_limit = 6;
+    motor.voltage_limit = 3;
     motor.voltage_sensor_align = 1;
 
-    // velocity low pass filtering time constant
-    // the lower the less filtered
     motor.LPF_velocity.Tf = 0.05;
-
-    // angle P controller
     motor.P_angle.P = 20;
-    // maximal velocity of the position control
     motor.velocity_limit = 12;
 
-    // motor.sensor_offset = 1.78;              // Set the zero electrical angle
-    motor.sensor_direction = Direction::CCW; // Set the sensor direction to counter-clockwise
+    motor.sensor_offset = 1.67;
+    motor.sensor_direction = Direction::CCW; // 设置传感器方向为逆时针
 
-    // use monitoring with serial
+    // 使用串口监控
     Serial.begin(115200);
-    // Serial1.begin(115200);
     Serial1.begin(115200, SERIAL_8N1, 18, 17);
-    // comment out if not needed
     motor.useMonitoring(Serial);
 
-    // initialize motor
+    // 初始化电机
     motor.init();
-    // align sensor and start FOC
-    // motor.();
     motor.initFOC();
 
-    _delay(100); // 等待主控制程序启动
+    // 创建 FOC 任务，固定到核心 0
+    xTaskCreatePinnedToCore(
+        focTask,
+        "FOC Task",
+        40960,
+        NULL,
+        2,
+        NULL,
+        0
+    );
 
     Serial.println(F("Motor ready"));
 }
 
 void loop()
 {
-    motor.loopFOC();
-    if(counter < 5000){
+    if(counter < 1000){
         counter = counter + 1;
         motor.controller = MotionControlType::torque;
         motor.move(1.5);
-        if(counter == 5000) motor.sensor_offset = -sensor.getAngle();
+        if(counter == 1000) motor.sensor_offset = -sensor.getAngle();
     }
     else {
         if (left_hand * motor.shaft_angle > angle_limit) {
-            // 超过限制时，切换到力矩控制并停止电机
             motor.controller = MotionControlType::torque;
             target = 0;
         } else {
-            // 未超过限制时，继续正常运动
+            error = target - motor.shaft_angle;
+            // Serial.println(motor.shaft_velocity);
+            if (abs(motor.shaft_velocity) < velocity_threshold && abs(error) > angle_threshold) {
+                motor.controller = MotionControlType::torque;
+                target = torque_limit;
+            }
+            else {
+                motor.controller = MotionControlType::angle;
+                target = left_hand * angle_target;
+            }
+
             motor.move(target);
         }
-        // Serial.println(motor.shaft_angle);
+
         if (Serial1.available() >= 16) {        
             std::vector<uint8_t> data(16);
             Serial1.readBytes(data.data(), 16);
             
-            // if (data[7] == 0x14) {
-            uint8_t mode = data[8];
             uint16_t value = (data[10] << 8) | data[11];
-            switch (mode) {
-                case 0: // 初始化
-                    break;
-                case 1: // 力矩控制
-                    motor.controller = MotionControlType::torque;
-                    target = left_hand * static_cast<float>(value) / 100.0;
-                    break;
-                case 2: // 角度控制
-                    motor.controller = MotionControlType::angle;
-                    target = left_hand * static_cast<float>(value) / 100.0;
-                    break;
-                case 3: // 角度控制带力矩限制
-                    float angle_target = static_cast<float>(value) / 100.0;
-                    uint16_t voltage_limit_value = (data[12] << 8) | data[13];
-                    float voltage_limit = static_cast<float>(voltage_limit_value) / 100.0;
-                    motor.controller = MotionControlType::angle;
-                    target = left_hand * angle_target;
-                    motor.voltage_limit = voltage_limit;
-                    break;
-                // }
-            }
+            angle_target = static_cast<float>(value) / 100.0;
+
+            uint16_t torque_limit_value = (data[12] << 8) | data[13];
+            torque_limit = static_cast<float>(torque_limit_value) / 100.0;          
         }
     }
+    delay(2);
 }
--- test/test.py
+++ test/test.py
@@ -1,12 +1,11 @@
-from CholoepusCtrl import CholoepusCtrl
-import time
 
+import time
+from CholoepusCtrl import CholoepusCtrl
+
+# 初始化控制器
 controller = CholoepusCtrl(port='/dev/tty.usbmodem92867CBE124B1')  # 替换为实际串口
-# controller.initialize()
-# controller.set_torque(1)  # 设置扭矩1.5
-# time.sleep(1)
-# controller.set_angle(10)  # 设置角度45度
-# time.sleep(1)
-controller.set_angle_with_torque_limit(40.0, 1.0)  # 设置角度30度，电压限制5
+
+controller.set_angle_with_torque_limit(90, 1)
 time.sleep(10)
-controller.close()
+controller.set_angle_with_torque_limit(10, 1)
+time.sleep(10)
